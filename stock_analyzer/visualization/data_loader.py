d97d84d6065fb79079d9fe30960415cce1fd653b
Cr0nn <kolegov2005@bk.ru>
2025-11-12 18:06:38 +0700

    Update data_loader.py
    Удалена не нужная часть кода

--- stock_analyzer/visualization/data_loader.py
+++ stock_analyzer/visualization/data_loader.py
@@ -5,12 +5,3 @@ import requests
 
 def get_sample_data(data):
     return pd.DataFrame(data)
-
-def get_moex_data(ticker):
-    url = f"https://iss.moex.com/iss/engines/stock/markets/shares/boards/TQBR/securities/{ticker}.json"
-    response = requests.get(url)
-    data = response.json()
-    if data['securities']['data']:
-        quotes = data['securities']['data'][0]
-        return float(quotes[3]), float(quotes[4])  # Цена, объём
-    return np.nan, np.nan
