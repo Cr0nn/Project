8bda4f6fd8b71ea72c98a088994d41b6105de7d1
Cr0nn <kolegov2005@bk.ru>
2025-11-12 18:08:59 +0700

    Update table_widget.py
    Изменение в поведении основной таблицы

--- stock_analyzer/visualization/table_widget.py
+++ stock_analyzer/visualization/table_widget.py
@@ -1,14 +1,15 @@
-from PyQt6.QtWidgets import QTableWidget, QTableWidgetItem
-from PyQt6.QtCore import Qt
+from PySide6.QtWidgets import QTableWidget, QTableWidgetItem, QHeaderView
+from PySide6.QtCore import Qt, QTimer
 import pandas as pd
 
 def create_table_widget():
     table = QTableWidget()
     table.setRowCount(0)
     table.setColumnCount(0)
-    table.setFixedWidth(500)
+
 
     return table
+
 
 def update_table_data(table, df):
     table.setRowCount(len(df))
@@ -19,5 +20,17 @@ def update_table_data(table, df):
         for j in range(len(df.columns)):
             item = QTableWidgetItem(str(df.iloc[i, j]))
             table.setItem(i, j, item)
+
+
     table.resizeColumnsToContents()
-    table.setVisible(True)
+
+    def adjust_width():
+        header = table.horizontalHeader()
+        total_width = sum(header.sectionSize(i) for i in range(table.columnCount()))
+        # добавляем рамки и микроотступы
+        total_width += table.verticalHeader().width() 
+        total_width += table.frameWidth() * 2  
+        total_width += 18  # небольшой запас, чтобы точно не обрезало
+        table.setFixedWidth(total_width)
+
+    QTimer.singleShot(0, adjust_width)
